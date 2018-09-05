5f8c18ce35e4ffdbb00ad056431f0c9b55f9561a
Tristan <tkidder132@gmail.com>
2018-09-04 23:06:53 -0500

    added options menu, updated music manager and build settings to include options scene, added options controller for setting difficulty and volume

---
+++ Assets/_Scripts/OptionsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public Slider difficultySlider;
+    public LevelManagerController levelManager;
+    private MusicManagerController musicManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        musicManager = GameObject.FindObjectOfType<MusicManagerController>();
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+        difficultySlider.value = PlayerPrefsController.GetDifficulty();
+	}
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsController.SetDifficulty(difficultySlider.value);
+        levelManager.LoadLevel("Start");
+    }
+}
